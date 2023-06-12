c3874e929112dae2fb13f1ab32c08f2ecae3c411
Otmars <otmars1998@gmail.com>
2023-06-12 16:28:56 -0400

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {GeolocationService} from '@ng-web-apis/geolocation';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'googleMaps';
+export class AppComponent implements OnInit {
+  constructor(private readonly geolocation$: GeolocationService) {}
+  ngOnInit(): void {
+    
+    this.getPosition()
+  }
+  title : any = 0
+  // title = navigator.geolocation.getCurrentPosition(position =>{
+  //   console.log(position);
+    
+  // })
+  getPosition() {
+    this.geolocation$.subscribe(position => {
+      console.log(position.coords);
+      this.title = position.coords
+    })
+     
+}
+
 }
